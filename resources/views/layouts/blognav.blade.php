0b98766c4d62a1f078c501b515c17d89a589917d
vagrant <vagrant@homestead>
2018-04-19 02:11:00 +0000

    Can not summit comment and register

--- resources/views/layouts/blognav.blade.php
+++ resources/views/layouts/blognav.blade.php
@@ -1,13 +1,19 @@
     <div class="blog-masthead">
       <div class="container">
         <nav class="nav blog-nav">
-          <a class="nav-link active" href="#">Home</a>
+          <a class="nav-link active" href="/">Home</a>
           <a class="nav-link" href="#">New features</a>
           <a class="nav-link" href="#">Press</a>
           <a class="nav-link" href="#">New hires</a>
 
+          
           @if (Auth::check())
-            <a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
+            <a class="nav-link" href="{{ url('blog/create') }}">Add new post</a>
+           <a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
+           <a class="nav-link" href="{{ url('logout') }}">Logout</a>
+          @else
+            <a class="nav-link ml-auto" href="{{ url('login') }}">Login</a>
+            <a class="nav-link" href="{{ url('register') }}">Register</a>
           @endif
 
         </nav>
